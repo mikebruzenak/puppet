5ad48640916e2d58845f0a7cfa3d41459760a533
mikebruzenak <mike.bruzenak@gmail.com>
2019-03-29 17:19:50 +0000

    added resources

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -4,8 +4,17 @@ file { '/usr/local/bin/run-puppet':
   mode   => '0755',
 }
 
+file { '/etc/puppetlabs/code/environments/testfileresource.txt':
+  ensure => file,
+  content => "whatever should\ngo here\nwill be sufficient\n"
+}
+
+file { '/etc/motd':
+  source => '/examples/puppet/files/motd.txt',
+}
+
 cron { 'run-puppet':
   command => '/usr/local/bin/run-puppet',
   hour    => '*',
-  minute  => '*/15',
+  minute  => '*/5',
 }
